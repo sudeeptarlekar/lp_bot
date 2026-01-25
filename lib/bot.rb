53303e0f50e91e79587b51ba57fbb3071cbe155e
Sudeep Tarlekar <sudeeptarlekar@gmail.com>
2026-01-25 22:49:41 +0100

    Add logic to load local data
    TheTrainLine has bot detection in place, so it is hard to scrap the
    data from web, so for POC static data is assumed.
    This commit adds static data from London to Paris and adds the data
    loading logic. Need to add parsing logic and then filtering out
    segments which are in previous time than departure time.

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 require 'httparty'
+require 'json'
+require 'time'
 
 require 'bot/the_train_line/base'
--- lib/bot/the_train_line/base.rb
+++ lib/bot/the_train_line/base.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 
 require_relative 'location'
+
+module Bot
+  module TheTrainLine
+    class << self
+      def find(from, to, departure_time)
+        json_data = fetch_data(from, to)
+      end
+
+      private
+
+      def fetch_data(from, to)
+        JSON.parse(File.read("./data/#{from.downcase}_to_#{to.downcase}.json"))['data']
+      rescue Errno::ENOENT => _e
+        puts "No journeys present between #{origin_name} to #{destination_name}"
+        exit(0)
+      end
+    end
+  end
+end
